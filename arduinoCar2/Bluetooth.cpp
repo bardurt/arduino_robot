e12a27e0f7f667896da3e75c4d50b60789391298
bardur <zygneapps@gmail.com>
2020-08-08 09:57:18 -0500

    clean up code

--- arduinoCar2/Bluetooth.cpp
+++ arduinoCar2/Bluetooth.cpp
@@ -1,6 +1,5 @@
 #include "Bluetooth.h"
 
-
 Bluetooth::Bluetooth(){}
 
 Bluetooth::Bluetooth(int _statePin){
@@ -10,21 +9,13 @@ Bluetooth::Bluetooth(int _statePin){
 
 bool Bluetooth::isConnected(){
 
-  if(digitalRead(statePin) == HIGH)
+  if(digitalRead(statePin) == HIGH){
     return true;
-  else
+  } else {
     return false;
+  }
 }
 
-/*String Bluetooth::readData(){
-  if(Serial.available() > 0){
-    String message = "";
-    message.concat(Serial.read());
-    return message;
-  }
-   else
-      return "";
-}*/
 
 void Bluetooth::sendMessage(String _message){
     Serial.println(_message);
@@ -32,14 +23,13 @@ void Bluetooth::sendMessage(String _message){
 
 void Bluetooth::readData(String &Message){
 
-Message = "";
-int i = 0;
-while(Serial.available() > 0)
-{
-  char in = Serial.read();
-  Message.concat(in);
-  return;
- }
+	Message = "";
+	int i = 0;
+	while(Serial.available() > 0) {
+  	  char in = Serial.read();
+  	  Message.concat(in);
+  	  return;
+ 	}
 }
 
 
